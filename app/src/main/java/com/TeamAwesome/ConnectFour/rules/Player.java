cf20c388c6514e9cfcaeda283fe0840951d4c9f5
Jordan Cantrell <cantrel2@pd.edu>
2018-02-18 08:50:57 -0800

    Nuked old code

--- app/src/main/java/com/TeamAwesome/ConnectFour/rules/Player.java
+++
@@ -1,6 +0,0 @@
-package com.TeamAwesome.ConnectFour.rules;
-
-public class Player {
-    public static final int PLAYER1 = 1;//you
-    public static final int PLAYER2 = 2;//Ai or friend
-}
--- app/src/main/java/com/TeamAwesome/ConnectFour/utils/Constants.java
+++
@@ -1,8 +0,0 @@
-package com.TeamAwesome.ConnectFour.utils;
-
-public class Constants {
-
-    public static final int AI_DELAY = 1000;
-
-    public static final int SPLASH_TIME_OUT = 2000;
-}
