ca8eee234960c4c3bd935a26c6d111bc1aadedf1
soyoung <soyoung@pdx.edu>
2018-02-26 23:26:25 -0800

    Updated MenuActivity and MenuController

--- app/src/main/java/com/TeamAwesome/ConnectFour/view/GamePlayActivity.java
+++ app/src/main/java/com/TeamAwesome/ConnectFour/view/GamePlayActivity.java
@@ -1,7 +1,12 @@
 package com.TeamAwesome.ConnectFour.view;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.text.InputType;
+import android.util.Log;
+import android.widget.EditText;
 
 import com.example.jorda.connect4.R;
 
@@ -15,4 +20,5 @@ public class GamePlayActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gameplay);
     }
+
 }
