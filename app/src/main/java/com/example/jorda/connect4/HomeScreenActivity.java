cf7220252637a92ae735672c910c20d0e9774904
Branden <bnelson3632@gmail.com>
2018-03-03 20:36:47 -0800

    new screens

--- app/src/main/java/com/example/jorda/connect4/HomeScreenActivity.java
+++ app/src/main/java/com/example/jorda/connect4/HomeScreenActivity.java
@@ -1,27 +1,40 @@
 package com.example.jorda.connect4;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.util.Log;
 
 public class HomeScreenActivity extends AppCompatActivity {
 
     private Button localGame;
+    private Button onlineGame;
+    private Button highScores;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
-        Log.wtf("Home Screen Activity", "OnCreate");
         localGame = findViewById(R.id.btnLocalGame);
-        Log.wtf("Home Screen Activity", "OnCreate");
+        onlineGame = findViewById(R.id.btnOnlineGame);
+        highScores = findViewById(R.id.btnHighScores);
         localGame.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(HomeScreenActivity.this, MenuActivity.class));
+            }
+        });
+        onlineGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(HomeScreenActivity.this, OnlineActivity.class));
+            }
+        });
+        highScores.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(HomeScreenActivity.this, HighScoreActivity.class));
             }
         });
     }
