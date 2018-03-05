74aa7e8e57a44b64a0fbab65969e4838923ab0c8
Jordan <cantrel2@pdx.edu>
2018-03-05 07:26:06 -0800

    got animation working

--- app/src/main/java/com/example/jorda/connect4/GamePlayActivity.java
+++ app/src/main/java/com/example/jorda/connect4/GamePlayActivity.java
@@ -1,7 +1,9 @@
 package com.example.jorda.connect4;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -18,25 +20,54 @@ import android.content.Intent;
 public class GamePlayActivity extends AppCompatActivity {
     private int mColumns;
     private int mRows;
+    private int mTops[]; // Current tops of each column
     private GamePlayController mController;
+    //final private GamePlayView mGamePlayView;
+    private View mGamePlayView;
+    private int currentRound;
+    private int mBoard[][];
 
 
-
+/*
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_gameplay);
+
+        Log.wtf("u wot m8?", "GamePlayActivity onCreate");
+        setContentView(R.layout.activity_game1);
 
         Intent intent = getIntent();
         MenuController menuController = (MenuController)intent.getSerializableExtra("MenuController");
 
+        setContentView(R.layout.activity_gameplay);
+        GamePlayView gamePlayView = (GamePlayView) findViewById(R.id.gameView1);
+
         //Test the chosen board size
-        Toast.makeText(GamePlayActivity.this,"Test - Board size : "+menuController.getBoard_size(),Toast.LENGTH_SHORT).show();
+        //Toast.makeText(GamePlayActivity.this,"Test : "+menuController.getPlayer2_color(),Toast.LENGTH_SHORT).show();
 
-    }
 
-    public int doMove(int column)
-    {
+        mColumns = menuController.getBoard_column();
+        mRows = menuController.getBoard_row();
+        mTops = new int[mColumns];
+        mBoard = new int[mColumns][mRows];
+        currentRound = menuController.getNum_rounds();
+        //mGamePlayView = (View) findViewById(R.id.gameViewRelative);
+        //mController = new GamePlayController(this, this, (GamePlayView) mGamePlayView);
 
-    }
+        Log.wtf("GamePlayActivity","before adjust");
+       //mGamePlayView.adjustSize(mColumns, mRows, mController);
+        Log.wtf("GamePlayActivity", "after adjust");
+*/
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            int board_width = 7;
+            int board_height = 6;
+            setContentView(R.layout.activity_game1);
+            GamePlayView boardView = (GamePlayView) findViewById ( R.id.gameView1 );
+            mController = new GamePlayController(this, boardView, board_width, board_height);
+            //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            //getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_action_close);
+        }
+
 }
--- app/src/main/java/com/example/jorda/connect4/GamePlayController.java
+++ app/src/main/java/com/example/jorda/connect4/GamePlayController.java
@@ -1,20 +1,69 @@
 package com.example.jorda.connect4;
 
+import android.app.AlertDialog;
+import android.content.Context;
 import android.support.annotation.NonNull;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 /**
  * Created by school on 2/28/2018.
  */
 
-public class GamePlayController {
-    private GamePlayActivity mGamePlay;
+public class GamePlayController implements View.OnClickListener {
+    private Context mContext;
+
+    public int COLS;
+    public int ROWS;
+    private int[][] mGrid;
+    private int[] mFree;
+    private int mPlayerTurn;
+
+    private GamePlayModel mGamePlay;
     private GamePlayView mPlayView;
-    private float cellWidth;
+
+    public GamePlayController(Context context, GamePlayView PlayView_in, int cols, int rows)
+    {
+        mContext = context;
+        this.COLS = cols;
+        this.ROWS = rows;
+        this.mContext = context;
+        mGrid = new int[ROWS][COLS];
+        mFree = new int[COLS];
+        mGamePlay = new GamePlayModel(mGrid, mFree);
+        mPlayerTurn = 1;
+        // null the mGrid and mFree counter for every column
+        for (int j = 0; j < COLS; ++j) {
+            for (int i = 0; i < ROWS; ++i) {
+                mGrid[i][j] = 0;
+            }
+            mFree[j] = ROWS;
+        }
+
+        //mGamePlay = GamePlay_in;
+        mPlayView = PlayView_in;
+        if (mPlayView != null) {
+            mPlayView.initialize(this, COLS, ROWS);
+        }
+    }
+
+    private void selectColumn(int column) {
+        if (mFree[column] == 0) {
+            return;
+        }
+
+        mGamePlay.placeMove(column, mPlayerTurn);
+        mPlayView.dropDisc(mFree[column], column, mPlayerTurn);
+        mPlayerTurn = mPlayerTurn == 1 ? 2 : 1;
+    }
 
     @Override
     public void onClick(@NonNull View v)
     {
-        mGamePlay.doMove( (int)v.getX() / (int) cellWidth);
+        int col = mPlayView.colAtX(v.getX());
+        selectColumn(col);
+
+        //mGamePlay.doMove( (int)v.getX() / (int) mPlayView.getCellWidth() );
     }
 }
