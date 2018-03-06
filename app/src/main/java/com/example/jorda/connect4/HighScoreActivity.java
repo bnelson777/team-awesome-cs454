a2c1ba3cc70fee942fc6425d62537253bb48d422
Branden <bnelson3632@gmail.com>
2018-03-05 21:11:35 -0800

    ip

--- app/src/main/java/com/example/jorda/connect4/HighScoreActivity.java
+++ app/src/main/java/com/example/jorda/connect4/HighScoreActivity.java
@@ -1,7 +1,10 @@
 package com.example.jorda.connect4;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
 
 public class HighScoreActivity extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class HighScoreActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_high_score);
+        SharedPreferences prefs = this.getSharedPreferences("Connect4HighScores", Context.MODE_PRIVATE);
+        int score1 = prefs.getInt("score1", 0);
+        int score2 = prefs.getInt("score2", 0);
+        int score3 = prefs.getInt("score3", 0);
+        int score4 = prefs.getInt("score4", 0);
+        int score5 = prefs.getInt("score5", 0);
+
     }
 }
